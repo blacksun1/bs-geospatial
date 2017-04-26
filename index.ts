92db564f06ee0fe4c60b775a4a06db5c4b63a17c
blacksun1 <simon@blacksun.cx>
2017-04-26 19:25:11 +0930

    Fix security issue

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 /// <reference path="./typings/node/node.d.ts" />
 
-let Q = require('q');
-let colors = require('colors');
-let util = require('util');
-let geojsonhint = require('geojsonhint');
+const Q = require('q');
+const colors = require('colors');
+const util = require('util');
+const geojsonhint = require('geojsonhint');
 let geoJsonFiles;
+
 
 /**
  * Returns true if the string str ends with the suffix.
@@ -12,74 +13,45 @@ let geoJsonFiles;
  * @param  {string} suffix - The suffix of the string to test for.
  * @return {bool}          - true if the string ends in the suffix. False if it doesn't.
  */
-let endsWith = function (str, suffix) {
+function endsWith(str: string, suffix: string) {
   return str.indexOf(suffix, str.length - suffix.length) !== -1;
-};
-
-/**
- * Returns a promise that eventually returns the contents of a file.
- * @param  {string} filename        - The name of the file to read.
- * @param  {string} [encoding=null] - The encoding to use when reading the file. Default is null.
- * @return {raw buffer}             - The contents of the file.
- */
-let readFilePromise = function (filename, encoding) {
-  return Q.Promise(function (resolve, reject, notify) {
-    var fs = require('fs');
-
-    // console.log(colors.cyan(`Reading from file ${filename}`));
-    fs.readFile(filename, { flag: 'r', encoding: encoding === undefined ? null : encoding }, function (err, data) {
-      if (err) {
-        return reject(err);
-      }
-
-      return resolve(data);
-    });
-  });
-};
-
-/**
- * Returns an array of strings containing the file names of the files at the path.
- * @param  {string} path - The folder path.
- * @return {string[]}    - An arry of strings containing the names of the files at the folder specified in path.
- */
-let readdirPromise = function (path) {
-  return Q.Promise(function (resolve, reject, notify) {
-    var fs = require('fs');
-
-    // console.log(colors.cyan(`Reading directory structure from folder ${path}`));
-    fs.readdir(path, function (err, data) {
-      if (err) {
-        return reject(err);
-      }
-
-      return resolve(data);
-    });
-  });
-};
+}
 
 /**
  * Filter an array of file names with all files with an extension of .geojson.
  * @param {string[]} files - The array of files to filter
  * @return {string[]}      - A filtered array of files.
  */
-let filterAllGeojsonFiles = function(files) {
+function filterAllGeojsonFiles(files: string[]) {
   if (!files || files.length === 0) {
     return null
   }
 
   return files.filter(function (folder) { return endsWith(folder, '.geojson'); });
-};
+}
+
+/**
+ * A generic catch function to be used in a promise chain. Just outputs an error message.
+ * @param  {Any} err - The object that was rejected as part of the chain.
+ * @return {void}
+ */
+function genericErrorCatcher(err) {
+  if (err.name && err.message) {
+    console.error(colors.red(`An exception has occured: ${err.name}, ${err.message}`));
+    return;
+  }
+
+  console.error(colors.red(err.message));
+}
 
 /**
  * A promise to eventually read and GeoJSON hint a file.
- * @param {string} file - The path of the file to be checked.
+ * @param {string} fileName - The path of the file to be checked.
  * @return {Promise}
  */
-let hintGeoJsonFile = function(file) {
+function hintGeoJsonFile(fileName: string) {
   return Q.Promise(function(resolve, reject, notify) {
-    let filePath = file;
-
-    return readFilePromise(filePath, "UTF-8")
+    return readFilePromise(fileName, "UTF-8")
       .then(function(content) {
         let fileContent = content;
         let contentObject;
@@ -89,8 +61,9 @@ let hintGeoJsonFile = function(file) {
           contentObject = JSON.parse(content)
         }
         catch (e) {
-          let err = new Error(`File ${filePath} could not be parsed as valid JSON.`);
+          const err = new Error(`File ${fileName} could not be parsed as valid JSON.`);
           err.name = 'App.InvalidJson'
+
           return reject(err);
         }
 
@@ -98,39 +71,68 @@ let hintGeoJsonFile = function(file) {
           parseErrors = geojsonhint.hint(contentObject);
         }
         catch (e) {
-          let err = new Error(`App.InvalidGeoJson: File ${filePath} could not be hinted with Geo JSON.`);
+          const err = new Error(`App.InvalidGeoJson: File ${fileName} could not be hinted with Geo JSON.`);
           err.name = 'App.InvalidGeoJson';
+
           return reject(err);
         }
 
         if (parseErrors.length > 0) {
-          let err = new Error();
+          const err = new Error();
           err.name = "App.InvalidGeoJson";
-          err.message = `File ${filePath} is invalid Geo JSON.`;
+          err.message = `File ${fileName} is invalid Geo JSON.`;
+
           return reject(err);
         }
 
-        console.log(colors.green(`✓ ${filePath}`));
+        console.log(colors.green(`✓ ${fileName}`));
         return resolve();
       })
   });
-};
+}
 
 /**
- * A generic catch function to be used in a promise chain. Just outputs an error message.
- * @param  {object} err - The object that was rejected as part of the chain.
- * @return {void}
+ * Returns a promise that eventually returns the contents of a file.
+ * @param  {string} filename        - The name of the file to read.
+ * @param  {string} [encoding=null] - The encoding to use when reading the file. Default is null.
+ * @return {raw buffer}             - The contents of the file.
  */
-let genericErrorCatcher = function (err) {
-  if (err.name && err.message) {
-    console.error(colors.red(`An exception has occured: ${err.name}, ${err.message}`));
-    return;
-  }
+function readFilePromise(filename: string, encoding: string) {
+  return Q.Promise(function(resolve: Function, reject: Function, notify: Function) {
+    var fs = require('fs');
 
-  console.error(colors.red(err.message));
-};
+    // console.log(colors.cyan(`Reading from file ${filename}`));
+    fs.readFile(filename, { flag: 'r', encoding: encoding === undefined ? null : encoding }, function(err, data) {
+      if (err) {
+        return reject(err);
+      }
 
-readdirPromise(process.cwd())
+      return resolve(data);
+    });
+  });
+}
+
+/**
+ * Returns an array of strings containing the file names of the files at the path.
+ * @param  {string} path - The folder path.
+ * @return {string[]}    - An arry of strings containing the names of the files at the folder specified in path.
+ */
+function readDirPromise(path: string) {
+  return Q.Promise(function(resolve, reject, notify) {
+    var fs = require('fs');
+
+    // console.log(colors.cyan(`Reading directory structure from folder ${path}`));
+    fs.readdir(path, function(err, data) {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(data);
+    });
+  });
+}
+
+readDirPromise(process.cwd())
   .then(filterAllGeojsonFiles)
 
   // Take the list of all files and add it to our page level variable.
